f3078cf823829d42772c76f853e13f06d42b72e9
Riqwan Thamir <riqwan.thahamir@n8n.io>
2026-05-22 15:54:37 +0200

    chore: fix lint

--- packages/@n8n/db/src/migrations/postgresdb/index.ts
+++ packages/@n8n/db/src/migrations/postgresdb/index.ts
@@ -185,8 +185,8 @@ import { AddNodeGroupsColumnToWorkflowAndHistory1784000000006 } from '../common/
 import { CreateInstanceAiCheckpointTable1784000000007 } from '../common/1784000000007-CreateInstanceAiCheckpointTable';
 import { ResetInstanceAiNativePersistence1784000000008 } from '../common/1784000000008-ResetInstanceAiNativePersistence';
 import { CreateAgentMemoryEntryTables1784000000009 } from '../common/1784000000009-CreateAgentMemoryEntryTables';
-import { CreateInstanceAiObservationTables1784000000011 } from '../common/1784000000011-CreateInstanceAiObservationTables';
 import { RefactorAgentObservationScope1784000000010 } from '../common/1784000000010-RefactorAgentObservationScope';
+import { CreateInstanceAiObservationTables1784000000011 } from '../common/1784000000011-CreateInstanceAiObservationTables';
 import type { Migration } from '../migration-types';
 
 export const postgresMigrations: Migration[] = [
